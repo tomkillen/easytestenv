42881045201f6ce87de950da0b5b758f5c6fad0a
Tom Killen <tomkillen@gmail.com>
2024-10-24 00:34:06 +0200

    Test apply resources
    Added tests to ensure apply resources works as expected

--- easytestenv/easytestenv_test.go
+++ easytestenv/easytestenv_test.go
@@ -6,6 +6,9 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/tomkillen/easytestenv/easytestenv"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 var _ = Describe("create test env", func() {
@@ -17,6 +20,16 @@ var _ = Describe("create test env", func() {
 		})
 		It("should create deployment", func() {
 			Expect(env.ApplyResources(filepath.Join("..", "deploy", "deployment"))).To(Succeed())
+		})
+		It("should have created the namespace test-namespace", func() {
+			var ns corev1.Namespace
+			Expect(env.Client.Get(env.Context, types.NamespacedName{Name: "test-namespace"}, &ns)).To(Succeed())
+			Expect(ns.Name).To(Equal("test-namespace"))
+		})
+		It("should have created the deployment test-namespace/nginx-deployment", func() {
+			var d appsv1.Deployment
+			Expect(env.Client.Get(env.Context, types.NamespacedName{Namespace: "test-namespace", Name: "nginx-deployment"}, &d)).To(Succeed())
+			Expect(d.Name).To(Equal("nginx-deployment"))
 		})
 		It("should shutdown when finished", func() {
 			env.Shutdown()
